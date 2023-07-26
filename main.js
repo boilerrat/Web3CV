c0656ed2dc06b02e357f99201828093563072b76
boilerrat <128boilerrat@gmail.com>
2023-07-26 06:03:15 -0400

    wallet attempt

--- main.js
+++ main.js
@@ -1,35 +1,11 @@
-// Fetch data from passport.json
-fetch('passport.json')
-  .then(response => response.json())
-  .then(data => {
-    // Get the table body where the data will be appended
-    const credentialDataElement = document.getElementById('credentialData');
-
-    // Iterate over each stamp in the data
-    data.stamps.forEach(stamp => {
-      const row = document.createElement('tr');
-
-      const providerCell = document.createElement('td');
-      providerCell.textContent = stamp.provider;
-      row.appendChild(providerCell);
-
-      const issuanceDateCell = document.createElement('td');
-      issuanceDateCell.textContent = stamp.credential.issuanceDate;
-      row.appendChild(issuanceDateCell);
-
-      const expiryDateCell = document.createElement('td');
-      expiryDateCell.textContent = stamp.credential.expirationDate;
-      row.appendChild(expiryDateCell);
-
-      const credentialTypeCell = document.createElement('td');
-      credentialTypeCell.textContent = stamp.credential.type.join(', ');
-      row.appendChild(credentialTypeCell);
-
-      const proofTypeCell = document.createElement('td');
-      proofTypeCell.textContent = stamp.credential.proof.type;
-      row.appendChild(proofTypeCell);
-
-      credentialDataElement.appendChild(row);
-    });
-  })
-  .catch(error => console.error('Error:', error));
+const provider = new WalletConnectProvider({
+    infuraId: "113308962c114124ba65dd93a5390a2e", // Required
+  });
+  
+  //  Enable session (triggers QR Code modal)
+  await provider.enable();
+  
+  //  Create Web3 instance
+  const web3 = new Web3(provider);
+  
+  
